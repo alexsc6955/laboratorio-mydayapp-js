fd4ac1749699b08265c66756aec962ff6f8b5160
Santiago <rincorpes@gmail.com>
2024-01-06 21:10:46 -0500

    Add Main component function

--- src/index.js
+++ src/index.js
@@ -3,4 +3,28 @@ import Paradox from "penrose-paradox"
 
 import { sayHello } from "./js/utils";
 
+function Main() {
+  const raw = {
+    tag: "section",
+    options: {
+      classList: "main",
+      children: [
+        {
+          tag: "ul",
+          options: {
+            classList: "main__list",
+          },
+        },
+      ],
+    }
+  }
+
+  const element = Paradox.buildElement(raw.tag, raw.options)
+
+  return {
+    raw,
+    element,
+  }
+}
+
 console.log(sayHello("Hello"));
